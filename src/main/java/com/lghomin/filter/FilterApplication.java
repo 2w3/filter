1f221a800b73f75a4c27c613bca38cb1bba79522
2w3 <hallo.sungho@gmail.com>
2021-07-09 16:05:19 +0900

    spring boot servlet filter

--- src/main/java/com/lghomin/filter/FilterApplication.java
+++ src/main/java/com/lghomin/filter/FilterApplication.java
@@ -2,12 +2,19 @@ package com.lghomin.filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FilterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilterApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World";
+	}
 }
